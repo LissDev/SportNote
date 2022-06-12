c65722b2c7757253253d5ccd0843c910abfb1e97
yaroslav.budanov <yaroslav.budanov@nw.rt.ru>
2022-06-12 14:21:07 +0300

    Initial commit

---
+++ app/src/main/java/ru/lissdev/sportnote/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.lissdev.sportnote
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
